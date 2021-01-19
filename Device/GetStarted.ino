ea5ecb9f43125e19f8f9e1a33277648d6926bdd6
Dylan K <50585244+DylanK46@users.noreply.github.com>
2021-01-19 15:55:43 +0000

    added endpoint streaming

--- Device/GetStarted.ino
+++ Device/GetStarted.ino
@@ -1,3 +1,7 @@
+// Copyright (c) Microsoft. All rights reserved.
+// Licensed under the MIT license. 
+// To get started please visit https://microsoft.github.io/azure-iot-developer-kit/docs/projects/connect-iot-hub?utm_source=ArduinoExtension&utm_medium=ReleaseNote&utm_campaign=VSCode
+
 #include "AZ3166WiFi.h"
 #include "AzureIotHub.h"
 #include "DevKitMQTTClient.h"
@@ -8,6 +12,7 @@
 #include "http_client.h"
 #include "OledDisplay.h"
 #include "Sensor.h"
+#include "RGB_LED.h"
 
 int messageCount = 1;
 int sentMessageCount = 0;
@@ -43,48 +48,6 @@ static int gAxesData[3];
 static unsigned char magSensorId;
 static int axes[3];
 
-const char SSL_CA_PEM[] = 	"-----BEGIN CERTIFICATE-----\n"
-							"MIIHOjCCBiKgAwIBAgIRANpZiZaYeDOQEOtqI6KfpjkwDQYJKoZIhvcNAQELBQAw\n"
-							"gY8xCzAJBgNVBAYTAkdCMRswGQYDVQQIExJHcmVhdGVyIE1hbmNoZXN0ZXIxEDAO\n"
-							"BgNVBAcTB1NhbGZvcmQxGDAWBgNVBAoTD1NlY3RpZ28gTGltaXRlZDE3MDUGA1UE\n"
-							"AxMuU2VjdGlnbyBSU0EgRG9tYWluIFZhbGlkYXRpb24gU2VjdXJlIFNlcnZlciBD\n"
-							"QTAeFw0yMDAyMDMwMDAwMDBaFw0yMjAyMDIyMzU5NTlaMBsxGTAXBgNVBAMMECou\n"
-							"aW50ZWdyb21hdC5jb20wggIiMA0GCSqGSIb3DQEBAQUAA4ICDwAwggIKAoICAQDd\n"
-							"A2PXF5NrzhgwbCMZiuOoyqrLre6c0G4HmgCqHKqwEHVrp4kP7/sacIUjuacZHGsQ\n"
-							"/3unc4tSnzcE+YdT0b6zu8zRitdZoMLFeFP6lPIpB9IYYlGKv8M9/OaGPX3Hjwpk\n"
-							"F+HDicVWQbNHg2OwaANuDMvfwX2YvW7IzEWk0Ct2Az7gapaTQTScLvAToZTEzYYs\n"
-							"2yMfslgzZkbxlM8q4MLnkbaPn/LcIo1Xrmi0JX/T25/EIMFLmgmfGI5GMx4tGWJy\n"
-							"hmxa+R9wqpdGWoFJtijxJNonzlMKRL4UQS7zvVcwj3DP1gtAi+v2M1TWBisds3Kf\n"
-							"a5t1jdmyUTBVr7W3ZqrNcM381h2J61OD3w57rRwSgoa+OIx6bwq4WAEEpYW0AFLk\n"
-							"YUkV4t120lwY2vUwFmJ6trjCfJi1l5ldck/4YTr4TM9cSP0GNnbhyV/9tOWT3z4p\n"
-							"WA95FeBYEfRlgfLrYM0eMGtufnLuPC3q/ZwNPSDyW+8S61jOhlp52YqbWLq4i1pu\n"
-							"6tSP+txjOTGYrF9PBvfd9qyJaT2b5wS4Im80wDy8wnKFtSE45genKmaV8fRdcRpm\n"
-							"R1RTKcT1MrmBH/PmYjbVC4pMjHmLtauk13/q6vqWJIm0e5Au5tCcSW3JwJEsYufx\n"
-							"rqlHfILhAcmpacZ42oA2FOkacOOf9ah5d7y6oaWcOQIDAQABo4IDAjCCAv4wHwYD\n"
-							"VR0jBBgwFoAUjYxexFStiuF36Zv5mwXhuAGNYeEwHQYDVR0OBBYEFE1PLqtBfwx3\n"
-							"2geg7ugiVoovShk2MA4GA1UdDwEB/wQEAwIFoDAMBgNVHRMBAf8EAjAAMB0GA1Ud\n"
-							"JQQWMBQGCCsGAQUFBwMBBggrBgEFBQcDAjBJBgNVHSAEQjBAMDQGCysGAQQBsjEB\n"
-							"AgIHMCUwIwYIKwYBBQUHAgEWF2h0dHBzOi8vc2VjdGlnby5jb20vQ1BTMAgGBmeB\n"
-							"DAECATCBhAYIKwYBBQUHAQEEeDB2ME8GCCsGAQUFBzAChkNodHRwOi8vY3J0LnNl\n"
-							"Y3RpZ28uY29tL1NlY3RpZ29SU0FEb21haW5WYWxpZGF0aW9uU2VjdXJlU2VydmVy\n"
-							"Q0EuY3J0MCMGCCsGAQUFBzABhhdodHRwOi8vb2NzcC5zZWN0aWdvLmNvbTArBgNV\n"
-							"HREEJDAighAqLmludGVncm9tYXQuY29tgg5pbnRlZ3JvbWF0LmNvbTCCAX4GCisG\n"
-							"AQQB1nkCBAIEggFuBIIBagFoAHcARqVV63X6kSAwtaKJafTzfREsQXS+/Um4havy\n"
-							"/HD+bUcAAAFwDRA+4wAABAMASDBGAiEA286Xo+dbQ0D+yJvvFGPMMUrQzu4n6IiU\n"
-							"ilsvKPwyMLUCIQDRC2BAZahopZfTvqswg+On1V57kyGiQow23Na0DEICGwB2AG9T\n"
-							"dqwx8DEZ2JkApFEV/3cVHBHZAsEAKQaNsgiaN9kTAAABcA0QPtEAAAQDAEcwRQIg\n"
-							"Ub4q+RDuMc1kiZDu8kxgrR/vkps6J5YhvWj9WWoSL+cCIQCw95NiAexPd8Jqv2ID\n"
-							"YSL43V/Hgx4Loc4g6gbfH5k5VQB1ACJFRQdZVSRWlj+hL/H3bYbgIyZjrcBLf13G\n"
-							"g1xu4g8CAAABcA0QPtIAAAQDAEYwRAIga3UvQQKoD/ugq6nwBwxtcDDy/gB16ihT\n"
-							"gbgeelHlfKUCIAw8uqAC2Wt59YW7ezfxJTeJc1cP1FDojL8LQMJv1oewMA0GCSqG\n"
-							"SIb3DQEBCwUAA4IBAQAL8l1I2pDz5bhhGQL8ElTHrHRuvc79CYAewoku5Y54cDHV\n"
-							"aB90oKQl/po6QSICnKbylJleS4PIK17jEvVKB4bLy9f8K5qzBZdn4sqbNqp6RLY8\n"
-							"cXCne+R7mMbG+LZsZgJiDdzRofBRQTVAaPvUxTe4xQXSluUB1ry0I4Hp/dBeYX19\n"
-							"/4g1J1CLmxg1kdT9Rt/xnlNTWjFb43qtlBBrEW7fszCVcGAFIVD2EfWZjumqGIUD\n"
-							"0n2PXsf15dr7B6EN5hxN9a6mlTSml0ZPfsS6gZfUnZ439mlo5TCZ2jhddGxSxJHE\n"
-							"iqE2sxsZcQ4jwkLPvOkNJg7psyQUiikKYApPowlS\n"
-							"-----END CERTIFICATE-----\n";
-
 //////////////////////////////////////////////////////////////////////////////////////////////////////////
 // Utilities
 
@@ -110,6 +73,8 @@ static void InitWifi()
 void setup()
 {
 
+  rgbLed.setColor(256, 256, 0);
+
   // Initialize the Serial and Screen
   Screen.init();
   Screen.print(2, "Initializing...");
@@ -134,7 +99,7 @@ void setup()
 
   // Initialize LEDs
   Screen.print(3, " > LEDs");
-  rgbLed.turnOff();
+  pinMode(45,OUTPUT);
   
   delay(100);
 
@@ -191,6 +156,7 @@ void loop()
 {
   if (hasWifi)
   {
+    
     //Serial.println("Serial Sensor Values");
 
     // Read temperature, humidity sensor
@@ -233,9 +199,66 @@ void loop()
 
     //Serial.println(" ");
 
-    void set_header(const char* key, const char* value);
-    HTTPClient *httpClient = new HTTPClient(SSL_CA_PEM, HTTP_POST, "https://hook.integromat.com/yfxl2timjw1e1ilhvqyqajbh6k31p8yo");
-    const Http_Response* result = httpClient->send();
+    HTTPClient *httpClient = new HTTPClient(HTTP_POST, "// @ADMIN ENTER ENDPOINT URL HERE!");
+
+    char xmlBuff[285];
+
+    char temp[sizeof(xmlBuff)];
+
+    strcpy(temp,"{"); 
+    
+    strcat(temp,"'temperature':"); 
+    strcat(temp,f2s(temperature, 3)); 
+    strcat(temp,",");
+
+    strcat(temp,"'humidity':"); 
+    strcat(temp,f2s(humidity, 3)); 
+    strcat(temp,","); 
+
+    strcat(temp,"'pressure':"); 
+    strcat(temp,f2s(pressure, 3)); 
+    strcat(temp,","); 
+
+    strcat(temp,"'AccelerometerX':"); 
+    strcat(temp,f2s(xAxesData[0],3)); 
+    strcat(temp,","); 
+
+    strcat(temp,"'AccelerometerY':"); 
+    strcat(temp,f2s(xAxesData[1],3)); 
+    strcat(temp,","); 
+
+    strcat(temp,"'AccelerometerZ':"); 
+    strcat(temp,f2s(xAxesData[2],3)); 
+    strcat(temp,",");
+
+    strcat(temp,"'GyroscopeX':"); 
+    strcat(temp,f2s(gAxesData[0],3)); 
+    strcat(temp,","); 
+
+    strcat(temp,"'GyroscopeY':"); 
+    strcat(temp,f2s(gAxesData[1],3)); 
+    strcat(temp,","); 
+
+    strcat(temp,"'GyroscopeZ':"); 
+    strcat(temp,f2s(gAxesData[2],3)); 
+    strcat(temp,",");
+
+    strcat(temp,"'MagnetometerX':"); 
+    strcat(temp,f2s(axes[0],3)); 
+    strcat(temp,","); 
+
+    strcat(temp,"'MagnetometerY':"); 
+    strcat(temp,f2s(axes[1],3)); 
+    strcat(temp,","); 
+
+    strcat(temp,"'MagnetometerZ':"); 
+    strcat(temp,f2s(axes[2],3)); 
+
+    strcat(temp,"}");
+
+    httpClient->set_header("Content-Type", "text/plain");
+
+    const Http_Response* result = httpClient-> send(temp, strlen(temp));
 
     if (result == NULL)
       {
@@ -243,20 +266,24 @@ void loop()
 
         Serial.print("Error Code: ");
         Serial.println(httpClient->get_error());
+        rgbLed.setColor(256,0,0);
       }
     else
       {
         delay(100);
-        Screen.print(2, "Success");
+        Screen.print(2, "Streaming");
 
         Serial.println("Body");
         Serial.println(result->body);
+        Serial.println(httpClient->get_error());
+        rgbLed.setColor(0,256,0);
       }
     delete httpClient;
 
     
   }
-  
-  delay(100);  // Every 0.1 second delay
+  digitalWrite(48,LOW);
+  delay(5000);  // Every 5 second delay
+  digitalWrite(48,HIGH);
 }
 
